0824a8b2e17797efa08c304619c70009246a1775
Sandeep Singh <singh.sandi10@gmail.com>
2023-10-15 22:13:17 -0400

    Automation Framework Selenium

---
+++ src/main/java/stepDefinitions/Contact_Us_Steps.java
@@ -0,0 +1,65 @@
+package stepDefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+
+public class Contact_Us_Steps {
+    private WebDriver driver;
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/java/drivers/chromedriver");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Given("I access the webdriver university contact us page")
+    public void i_access_the_webdriver_university_contact_us_page() {
+        driver.get("https://webdriveruniversity.com/Contact-Us/contactus.html");
+
+    }
+    @When("I enter the first name")
+    public void i_enter_the_first_name() {
+        driver.findElement(By.cssSelector("[name=\"first_name\"]")).sendKeys("Joe");
+    }
+    @And("I enter the last name")
+    public void i_enter_the_last_name() {
+        driver.findElement(By.cssSelector("[name=\"last_name\"]")).sendKeys("Blogs");
+    }
+    @And("I enter an email address")
+    public void i_enter_an_email_address()  {
+        driver.findElement(By.name("email")).sendKeys("singh.sandi10@gmail.com");
+    }
+    @And("I enter a comment")
+    public void i_enter_a_comment() {
+        driver.findElement(By.name("message")).sendKeys("Hello how are you?");
+    }
+    @And("I click on the submit button")
+    public void i_click_on_the_submit_button() {
+        driver.findElement(By.cssSelector("[value=\"SUBMIT\"]")).click();
+    }
+    @Then("I should be presented with a successful contact us submission message")
+    public void i_should_be_presented_with_a_successful_contact_us_submission_message() {
+        WebElement contactUs_Submission_Message = driver.findElement(By.xpath("//div[@id='contact_reply']/h1"));
+        Assert.assertEquals(contactUs_Submission_Message.getText(),"Thank You for your Message!");
+    }
+}
